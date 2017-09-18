f7f1fb785ed22e8d38fe41002f7a515047b5cfad
suweiqing <2638519660@qq.com>
2017-09-18 14:31:03 +0800

    路由文件搭建完毕

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
-const index = require('./routes/index');
-const users = require('./routes/users');
+//引入路由规则的文件
+const routes = require('./routes');
 
 const app = express();
 
@@ -23,8 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(routes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
---
+++ routes/home.js
@@ -0,0 +1,6 @@
+/**
+ * Created by su on 2017/9/18.
+ */
+exports.index = (req, res, next) => {
+    res.send('hello world');
+}
